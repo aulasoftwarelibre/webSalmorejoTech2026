eb148bbbb8917dc10ff0ff5a299c8772bf0e0ddc
ByteKata08 <melladoarenasjuan@gmail.com>
2025-11-25 17:45:57 +0100

    NOTIFÍCAME button and FECHA POR ANUNCIAR done, responsive

--- src/app/components/body/body.tsx
+++ src/app/components/body/body.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import styles from "./body.module.css";
 import logoSalmorejo from "../../assets/logoSalmorejo.png";
+import { style } from "framer-motion/client";
 
 const IMAGE_WIDTH = 1508;
 const IMAGE_HEIGHT = 241;
@@ -21,6 +22,17 @@ export default function body() {
         <h1 className={styles.text}>
           LA CITA DE LA TECNOLOGÍA CORDOBESA REGRESA CON UNA ENERGÍA IMPARABLE
         </h1>
+        <h1 className={styles.text2}>
+          FECHA POR ANUNCIAR
+        </h1>
+        <a
+          href="https://docs.google.com/forms/d/e/1FAIpQLSdNOP1mYrdZi2WQhaiZ-nMbRqYowmwW1T4RYTBlrnShJ-ukxA/viewform"
+          target="_blank"
+          rel="noopener noreferrer"
+          className={styles.notifybutton}
+        >
+          NOTIFÍCAME
+        </a>
         <a
           href="https://docs.google.com/forms/d/e/1FAIpQLSdNOP1mYrdZi2WQhaiZ-nMbRqYowmwW1T4RYTBlrnShJ-ukxA/viewform"
           target="_blank"
