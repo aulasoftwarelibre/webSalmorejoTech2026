27db5aae37aa0276db263ef9a363728bb94cb784
Zch13 <zchspain@gmi+ail.com>
2026-02-26 21:17:32 +0100

    Mapa del Salmorejo

--- src/app/components/findUs/findUs.tsx
+++ src/app/components/findUs/findUs.tsx
@@ -1,9 +1,19 @@
 import styles from "./findUs.module.css";
+import Image from "next/image";
+import map from "../../assets/map.png";
 
 export default function FindUs() {
   return (
     <div className={styles.container}>
       <p className={styles.findUs}>ENCUENTRANOS</p>
+      <div className={styles.mapContainer}>
+            <Image 
+              src={map}
+              alt="maps location"
+              className={styles.mapImage}
+            />
+        </div>
+
     </div>
   );
 }
