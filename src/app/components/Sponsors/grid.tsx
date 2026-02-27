31b26ab8954dda51ed998fd16daf49a1b1a9f514
Serpal64 <serpallop64@gmail.com>
2026-02-27 17:30:28 +0100

    feat: need to finish patrocinadores text, and some other tweaks, i have to go (for now)😃

--- src/app/components/Sponsors/grid.tsx
+++ src/app/components/Sponsors/grid.tsx
@@ -19,9 +19,9 @@ export const SponsorsGrid = ({ sponsors }: SponsorsGridProps) => {
 
   return (
     <Fragment>
-      <h1 className={`${styles.schedule} pt-5`}>PATROCINADORES</h1>
+      <h1 className={`${styles.schedule} bg-white/50 backdrop-blur-xs border border-neutral-400/20 pt-10`}>PATROCINADORES</h1>
       <div className={styles.background}>
-        <div className={styles.container}>
+        <div className={`${styles.container} bg-white/50 backdrop-blur-md border border-neutral-400/20`}>
           {orderedSponsors.map((sponsor) => {
             return (
               <Sponsor
