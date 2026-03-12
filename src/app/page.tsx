299f1ffb6ea5c92cbea0f4d4b05d677f5eb111ae
David19 <davidsanchezpalomino200603@gmail.com>
2026-03-12 17:57:18 +0100

    lots of fixes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,8 +4,7 @@ import Navbar from "@/app/components/navbar/navbar";
 import Body from "@/app/components/body/body";
 import Footer from "@/app/components/footer/footer";
 import Timetable from "@/app/components/Timetable/Timetable";
-import Cards from "@/app/components/Cards/Cards"
-
+import Cards from "./components/Cards/Cards";
 
 import { motion } from "framer-motion";
 import { useState } from "react";
@@ -18,7 +17,39 @@ export default function Home() {
   const [isAnimationComplete, setIsAnimationComplete] = useState(false);
   return (
     <div className="relative" style={{ minHeight: "100vh" }}>
-      <Cards/>
+      <motion.div
+      className="relative z-10 flex flex-col flex-grow"
+        initial="hidden"
+        animate="visible"
+        onAnimationComplete={() => setIsAnimationComplete(true)}
+        variants={{
+          hidden: {
+            scale: 0.8,
+            opacity: 0,
+            y: 50,
+          },
+          visible: {
+            scale: 1,
+            opacity: 1,
+            transition: {
+              delay: 0.5,
+              duration: 1.25,
+            },
+            y: 0,
+          },
+        }}
+        style={{ position: "relative", zIndex: 10 }}
+      >
+
+        <Navbar />
+        <Body />
+        <Timetable />
+        <FindUs/>
+        <SponsorsGrid sponsors={sponsors as SponsorProps[]} />
+        <Cards></Cards>
+        <Footer isVisible={isAnimationComplete} />
+
+      </motion.div>
     </div>
   );
 }
