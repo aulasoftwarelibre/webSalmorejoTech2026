e0cd14b50aa03f4262e83c25de1881a3cca25ba5
Darío Martínez Kostyuk <dariomartk@gmail.com>
2026-03-21 14:46:42 +0100

    feat: created carrusel of images on descripcion

--- src/app/components/description/description.tsx
+++ src/app/components/description/description.tsx
@@ -1,9 +1,12 @@
-import React from 'react';
-import Image from 'next/image';
-import styles from './description.module.css';
-import asistenteImg from '../../assets/asistentes.png';
-import ponenciasImg from '../../assets/ponencias.png';
-import networkingImg from '../../assets/networking.png';
+import React from "react";
+import Image from "next/image";
+import styles from "./description.module.css";
+import asistenteImg from "../../assets/asistentes.png";
+import ponenciasImg from "../../assets/ponencias.png";
+import networkingImg from "../../assets/networking.png";
+import startImg from "../../assets/carrusel/start.jpeg";
+import teamImg from "../../assets/carrusel/team.jpg";
+import networkingCarruselImg from "../../assets/carrusel/networking.jpg";
 
 interface DescriptionProps {
   title?: string;
@@ -22,20 +25,17 @@ const Description: React.FC<DescriptionProps> = ({
   location = "Rectorado",
   assistants = 400,
   talks = 10,
-  sponsors = ["patrocinadores", "Aula de Software Libre"]
+  sponsors = ["patrocinadores", "Aula de Software Libre"],
 }) => {
-
   const fullDescription = `${description} Este ${date} os esperamos en el ${location}. Gracias a nuestros ${sponsors[0]} y al ${sponsors[1]} por hacerlo posible.`;
 
   return (
     <div className={styles.container}>
-
       <div className={`${styles.glassCard}`}>
         <h2 className={styles.title}>{title}</h2>
       </div>
 
       <div className={`${styles.glassCard} ${styles.stats}`}>
-
         <div className={styles.stat}>
           <Image
             src={asistenteImg}
@@ -79,13 +79,51 @@ const Description: React.FC<DescriptionProps> = ({
             <div>Networking</div>
           </div>
         </div>
+      </div>
 
+      <div className={`${styles.glassCard} ${styles.carousel}`}>
+        <div className="logos group relative overflow-hidden flex py-10 [mask-image:_linear-gradient(to_right,_transparent_0,_white_128px,white_calc(100%-128px),_transparent_100%)]">
+          <div className="animate-slide-left group-hover:[animation-play-state:paused] flex shrink-0 w-max">
+            <img
+              className="mx-4 inline h-80 border-4 border-white rounded-3xl"
+              src={startImg.src}
+              alt="start"
+            />
+            <img
+              className="mx-4 inline h-80 border-4 border-white rounded-3xl"
+              src={teamImg.src}
+              alt="team"
+            />
+            <img
+              className="mx-4 inline h-80 border-4 border-white rounded-3xl"
+              src={networkingCarruselImg.src}
+              alt="networking"
+            />
+          </div>
+
+          <div className="animate-slide-left group-hover:[animation-play-state:paused] flex shrink-0 w-max">
+            <img
+              className="mx-4 inline h-80 border-4 border-white rounded-3xl"
+              src={startImg.src}
+              alt="start"
+            />
+            <img
+              className="mx-4 inline h-80 border-4 border-white rounded-3xl"
+              src={teamImg.src}
+              alt="team"
+            />
+            <img
+              className="mx-4 inline h-80 border-4 border-white rounded-3xl"
+              src={networkingCarruselImg.src}
+              alt="networking"
+            />
+          </div>
+        </div>
       </div>
 
       <div className={`${styles.glassCard} ${styles.content}`}>
         <p className={styles.fullText}>{fullDescription}</p>
       </div>
-
     </div>
   );
 };
