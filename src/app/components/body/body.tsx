768a2e7ad8cd7759507384131b3761bfb8ce49d4
Darío Martínez Kostyuk <dariomartk@gmail.com>
2026-05-12 08:56:10 +0200

    fix: no more streaming

--- src/app/components/body/body.tsx
+++ src/app/components/body/body.tsx
@@ -9,26 +9,6 @@ export default function body() {
           LA CITA DE LA TECNOLOGÍA CORDOBESA REGRESA CON UNA ENERGÍA IMPARABLE
         </h1>
         <h1 className={styles.text2}>8 DE MAYO DE 2026</h1>
-        <h1 className={styles.text}>¡Estamos en directo!</h1>
-
-        <div className={styles.butonsContainer}>
-          <a
-            href="https://www.youtube.com/live/1fJF0AjVHmg?is=6O3ID4-zd3nfqtSc"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.miboton}
-          >
-            Streaming OMEYA
-          </a>
-          <a
-            href="https://www.youtube.com/live/VVmXjBXRnn0?is=SDkDleWdeMGMQQ3D"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.miboton}
-          >
-            Streaming Mudéjar
-          </a>
-        </div>
       </div>
     </div>
   );
